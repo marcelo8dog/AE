a8cbd50556e42ecf04dee637a70a6aa53a2f227d
unknown <fer_sanchez_25_07@hotmail.com>
2017-06-28 13:22:17 -0500

    empieza la coneccion

--- programs/inicio.php
+++ programs/inicio.php
@@ -1,5 +1,15 @@
 <?php
+  include("conection.php")
   $usuario=$_POST['usuario'];
   $contra=$_POST['contra'];
-  header('Location: acceso.html');
+  mysqli_real_escape_string($usuario);
+  mysqli_real_escape_string($cuenta);
+  $res=mysqli_query(conection(),"SELECT * FROM usuario WHERE cuenta==".$usuario.";");
+  $fila=mysqli_fetch_assoc($res)
+  if($res===NULL)
+     echo "usuario o contraseña invalido";
+    else{
+      echo "window.location = 'index.html';";
+      echo $usuario.$contra;
+    }
 ?>
