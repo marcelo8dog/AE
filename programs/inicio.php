8409a5b9c78522fce7d3808e52dd5e1afcbdb030
unknown <fer_sanchez_25_07@hotmail.com>
2017-06-29 13:43:03 -0500

    try

--- programs/inicio.php
+++ programs/inicio.php
@@ -1,15 +1,27 @@
 <?php
-  include("conection.php")
-  $usuario=$_POST['usuario'];
-  $contra=$_POST['contra'];
-  mysqli_real_escape_string($usuario);
-  mysqli_real_escape_string($cuenta);
+  include("conection.php");
+  $usuarior=$_POST['usuario'];
+  $contrar=$_POST['contra'];
+  $usuario=mysqli_real_escape_string(conection(),$usuarior);
+  $contra=mysqli_real_escape_string(conection(),$contrar);
+  echo $usuario.$contra;
   $res=mysqli_query(conection(),"SELECT * FROM usuario WHERE cuenta==".$usuario.";");
-  $fila=mysqli_fetch_assoc($res)
-  if($res===NULL)
-     echo "usuario o contraseña invalido";
+  if($res==NULL)
+     echo "usuario incorrecto";
     else{
-      echo "window.location = 'index.html';";
-      echo $usuario.$contra;
+      $fila=mysqli_fetch_assoc($res);
+      echo $fila;
+      if($fila[password]!=$contra)
+      {
+          echo "contraseña invalida";
+      }
+      else{
+        session_start();
+        $_SESSION['usuario']=$usuario;
+        echo "true";
+        // header('Location: http://localhost/AE/acceso.html');
+        echo "location.href ='http://localhost/AE/acceso.html'";
+        // echo $usuario.$contra;
+      }
     }
 ?>
